b8a2e8ff111f3c64916e7779a667495edbcfbe65
封晔 <578797371@qq.com>
2017-11-25 09:55:28 +0800

    测试

---
+++ Train.WebAPI/Controllers/ProcessesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Train.WebAPI.Models;
+
+namespace Train.WebAPI.Controllers
+{
+    public class ProcessesController : ApiController
+    {   
+        public ProcessCollectionState Get(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return new ProcessCollectionState
+            {
+                Processes = Process
+                        .GetProcessesByName(name)
+                        .Select(p => new ProgressState(p))
+            };
+        }
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Train.WebAPI/Models/ProgressState.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Train.WebAPI.Models
+{   
+    /// <summary>
+    /// 进程状态
+    /// </summary>
+    public class ProgressState
+    {   
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 总毫秒数
+        /// </summary>
+        public double TotalProcessorTimeInMillis { get; set; }
+
+        public ProgressState() { }
+        public ProgressState(Process proc)
+        {
+            Id = proc.Id;
+            Name = proc.ProcessName;
+            TotalProcessorTimeInMillis = proc.TotalProcessorTime.TotalMilliseconds;
+        }
+    }
+
+    /// <summary>
+    /// 进程状态集合对象
+    /// </summary>
+    public class ProcessCollectionState
+    {   
+        /// <summary>
+        /// 进程状态
+        /// </summary>
+         public IEnumerable<ProgressState> Processes { get; set; }
+    }
+
+}
